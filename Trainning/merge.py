70a3c6ca9bde6b1fa6e81d0f0af2055556c11bb9
Congyaob <126852174+Congyaob@users.noreply.github.com>
2025-08-17 20:00:24 +0930

    merge.py

--- Trainning/merge.py
+++ Trainning/merge.py
@@ -12,14 +12,15 @@ for file in csv_files:
         df = pd.read_csv(file_path, low_memory=False)
         df["source_file"] = file  
         combined_df_list.append(df)
-        print(f"✔ Loaded: {file} — shape: {df.shape}")
+        print(f"Loaded: {file} — shape: {df.shape}")
     except Exception as e:
-        print(f"❌ Failed to load {file}: {e}")
+        print(f"Failed to load {file}: {e}")
 
 combined_df = pd.concat(combined_df_list, ignore_index=True)
-print(f"\n✅ The merge is completed, the total number of samples：{len(combined_df)}，Number of features：{len(combined_df.columns)}")
+print(f"\n The merge is completed, the total number of samples：{len(combined_df)}，Number of features：{len(combined_df.columns)}")
 
 output_file = os.path.join(data_dir, "cicids2017_combined.csv")
 combined_df.to_csv(output_file, index=False)
-print(f"📁 Saved as：{output_file}")
+print(f"Saved as：{output_file}")
+
 
